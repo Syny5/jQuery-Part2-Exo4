ccdb5d14c008641fa3bab4c425fefb33337165de
syn <vicdemilly@gmail.com>
2019-04-23 15:27:36 +0200

    Ajout de ready et de commentaires

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-$('.color').on('click', function(){
-  var colorChange = $(this).attr('class').split(' ')[1];
-  $('#text').css('color', colorChange);
+$(document).ready(function(){
+  $('.color').on('click', function(){
+    // split crée un tableau à partir d'une chaîne de caractère
+    // [1] parce que l'indice de l'élément correspond au deuxième élement du tableau
+    var colorChange = $(this).attr('class').split(' ')[1];
+    $('#text').css('color', colorChange);
+  });
 });
